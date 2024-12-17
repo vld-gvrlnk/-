383f16da3ab512b62fcb9d1a438b0d701dc5484b
vld-gvrlnk <aboba.gey8642@gmail.com>
2024-12-17 18:53:48 +0700

    Add files via upload

---
+++ множества.py
@@ -0,0 +1,8 @@
+my_set={1,52,1,52,52,52,1488 ,'беброид','почемуясдаюегэпоинформатике'}
+print(my_set)
+print(my_set.add('amogus'))
+print(my_set.add('хахаХА'))
+print(my_set)
+print(my_set.discard(1))
+print(my_set)
+
---
+++ словари.py
@@ -0,0 +1,8 @@
+my_dict={'Vlada':89628413844, 'Leopold':89039043247,'Nekit':89137192338}
+print(my_dict)
+print(my_dict.get('Nekit'))
+print(my_dict.get('Xzz'))
+my_dict.update({'Sojitel':89139056280,'Lerok':89061937315})
+del my_dict['Sojitel']
+print(my_dict['Lerok'])
+print(my_dict)
